6d5b5e0ac1cccaff86f93e856a05d3b63dc72775
kuige <635615580@qq.com>
2020-06-16 21:55:56 +0800

    添加初始文件

---
+++ HelloGit/src/main/java/com/kuige/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.kuige.git;
+
+/**
+ * @author ：kui
+ * @description：${description}
+ * @date ：Created in 2020/6/16 21:36
+ * @modified By：
+ * @version: $version$
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
